c9f0e9e64228c5bed35c4f95079f8ec213ecdb0a
Kushagra Mishra <68068074+P01S0N128@users.noreply.github.com>
2022-06-28 22:58:56 +0530

    Updated RGA to give N brightest stars
    Updated the RGA C code to return only the N brightest stars.

--- Feature Extraction/Region Growth/RGA.c
+++ Feature Extraction/Region Growth/RGA.c
@@ -19,13 +19,16 @@
 #define SKIP_PIXELS 2
 #define LENGTH 808
 #define BREADTH 608
+#define PIXEL_WIDTH 4.8e-6
+#define NUM_MAX_STARS 10
 
 int tot_stars=0;
-unsigned short star_num = 0;
-unsigned long x_sum[MAX_STARS] = {0};
-unsigned long y_sum[MAX_STARS] = {0};
-unsigned long pixel_sum[MAX_STARS] = {0};
-unsigned short num_pixels[MAX_STARS] = {0};
+// unsigned short star_num = 0;
+// unsigned long x_sum[MAX_STARS] = {0};
+// unsigned long y_sum[MAX_STARS] = {0};
+// unsigned long pixel_sum[MAX_STARS] = {0};
+// unsigned short num_pixels[MAX_STARS] = {0};
+unsigned long pixel_track[MAX_STARS] = {0};
 unsigned short i,j;
 
 unsigned short k;
@@ -33,91 +36,103 @@ unsigned short valid_stars = 0;
 double centroids_st[MAX_STARS][3];
 //Number of test cases
 unsigned short n=1;
-	
-void getData(unsigned short i, unsigned short j, unsigned short star_num, unsigned long x_sum[], unsigned long y_sum[], unsigned long pixel_sum[], unsigned short num_pixels[], short arr_out_img[BREADTH + 2][LENGTH + 2])
 
-{
-	// base case
-	if(arr_out_img[j][i] <= THRESHOLD)
-		return;
-	
-	// keeping track of the sums
-	x_sum[star_num] += (unsigned long)arr_out_img[j][i]*i;
-	y_sum[star_num] += (unsigned long)arr_out_img[j][i]*j;
-	pixel_sum[star_num] += (unsigned long)arr_out_img[j][i];
-	num_pixels[star_num] += 1;
-	arr_out_img[j][i] = 0;
-
-	// recursive calls
-	getData(i - 1, j, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
-	getData(i + 1, j, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
-	getData(i, j - 1, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
-	getData(i, j + 1, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
-
-	return;
-}
-
-
-void regionGrowth(short arr_out_img[BREADTH + 2][LENGTH + 2], double centroids_st[MAX_STARS][3])
-{
-	
-	// clear out all arrays
-	unsigned short stars = 0;
-	for(stars = 0; stars < MAX_STARS; stars++){
-		x_sum[stars] = 0;
-		y_sum[stars] = 0;
-		pixel_sum[stars] = 0;
-		num_pixels[stars] = 0;
-	}
-
-	//*uint64_t t1 = cortos_get_clock_time();
-	star_num = 0;
-	valid_stars = 0;
-	tot_stars = 0;
-	i = 0;
-	j = 0;
-	k = 0;
-	// cortos_printf("RGA output starts here\n");
-
-	for(j = 1; j < BREADTH + 1; j += SKIP_PIXELS)
-		for( i = 1; i < LENGTH + 1; i += SKIP_PIXELS)
-			if(arr_out_img[j][i] > THRESHOLD)
-			{
-				getData(i, j, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
-				star_num++;
+void sort(double centroids_st[][3], unsigned long pixel_track[MAX_STARS], int tot_stars){
+	double temp_st[3];
+	unsigned long temp_pt;
+	for (int i = 0; i < tot_stars; i++){
+		for (int j = 0; j < tot_stars-i-1; j++){
+			if(pixel_track[j] < pixel_track[j + 1]){
+				temp_pt = pixel_track[j];
+				pixel_track[j] = pixel_track[j + 1];
+				pixel_track[j + 1] = temp_pt;
+				for (int k = 0; k < 3; k++){
+					temp_st[k] = centroids_st[j][k];
+					centroids_st[j][k] = centroids_st[j + 1][k];
+					centroids_st[j + 1][k] = temp_st[k];
+				}
 			}
-
-
-	// cortos_printf("SSP_ID,Sensor_y_Pixels,Sensor_z_Pixels\n");
-	
-	for(k = 0; k < star_num; k++)
-	{
-		if((num_pixels[k] <= STAR_MAX_PIXEL) & (num_pixels[k] > STAR_MIN_PIXEL))
-		{
-			valid_stars++;
-			centroids_st[valid_stars-1][0] = valid_stars;
-			centroids_st[valid_stars-1][1] = (double)x_sum[k]/(double)pixel_sum[k] - ((double)(LENGTH/2) + 0.5);
-			centroids_st[valid_stars-1][2] = -1*((double)y_sum[k]/(double)pixel_sum[k] - ((double)(BREADTH/2) + 0.5));
-			
-			// To print Star ID, x coordinate, y coordinate
-			// cortos_printf("%.6f,%.6f,%.6f\n",centroids_st[k][0],centroids_st[k][1],centroids_st[k][2]);		
 		}
 	}
-	tot_stars += valid_stars;
-
-	/*uint64_t t2 = cortos_get_clock_time();
-	uint32_t t11 = t1& (0xffffffff);
-	uint32_t t12 = t2& (0xffffffff);
-	cortos_printf("RGA output ends here\n");
-	cortos_printf("time = %u \n",t12-t11);*/
-    printf("Total Stars: %d\n\n", tot_stars);
-	return;
 }
+	
+void getData(unsigned short p_i, unsigned short p_j, unsigned short* star_num, unsigned long x_sum[], unsigned long y_sum[], unsigned long pixel_sum[], unsigned short num_pixels[], short arr_out_img[BREADTH + 2][LENGTH + 2]){
+    // base case
+    if (arr_out_img[p_j][p_i] <= THRESHOLD)
+        return;
+
+    // keeping track of the sums
+    x_sum[*star_num] += (unsigned long)arr_out_img[p_j][p_i] * p_i;
+    y_sum[*star_num] += (unsigned long)arr_out_img[p_j][p_i] * p_j;
+    pixel_sum[*star_num] += (unsigned long)arr_out_img[p_j][p_i];
+    num_pixels[*star_num] += 1;
+    arr_out_img[p_j][p_i] = 0;
+
+    // recursive calls
+    getData(p_i - 1, p_j, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
+    getData(p_i + 1, p_j, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
+    getData(p_i, p_j - 1, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
+    getData(p_i, p_j + 1, star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
+
+    return;
+}
+
+
+
+void regionGrowth(short arr_out_img[BREADTH + 2][LENGTH + 2], double centroids_st[MAX_STARS][3], int* tot_stars)
+{
+    int valid_stars = 0;
+    
+    unsigned short star_num = 0;
+    unsigned long x_sum[MAX_STARS] = {0};
+    unsigned long y_sum[MAX_STARS] = {0};
+    unsigned long pixel_sum[MAX_STARS] = {0};
+    unsigned short num_pixels[MAX_STARS] = {0};
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    for (j = 1; j < BREADTH + 1; j += SKIP_PIXELS)
+        for (i = 1; i < LENGTH + 1; i += SKIP_PIXELS)
+            if (arr_out_img[j][i] > THRESHOLD)
+            {
+                getData(i, j, &star_num, x_sum, y_sum, pixel_sum, num_pixels, arr_out_img);
+                star_num++;
+            }
+    
+    for (k = 0; k < star_num; k++)
+    {   
+        
+        if ((num_pixels[k] <= STAR_MAX_PIXEL) & (num_pixels[k] > STAR_MIN_PIXEL))
+        {
+            valid_stars++;
+            centroids_st[valid_stars-1][0] = valid_stars;
+            centroids_st[valid_stars-1][1] = ((double)x_sum[k] / (double)pixel_sum[k] - ((double)(LENGTH / 2) + 0.5)) * PIXEL_WIDTH;
+            centroids_st[valid_stars-1][2] = (-1 * ((double)y_sum[k] / (double)pixel_sum[k] - ((double)(BREADTH / 2) + 0.5))) * PIXEL_WIDTH;
+			pixel_track[valid_stars-1] = pixel_sum[k];
+        }
+    }
+
+    *tot_stars += valid_stars;
+	sort(centroids_st, pixel_track, *tot_stars);
+	if (*tot_stars > NUM_MAX_STARS)
+		*tot_stars = NUM_MAX_STARS;
+    return;
+}
+
 
 void main()
 {
     printf("Image 1 output\n");
-	regionGrowth(arr_out_img, centroids_st);
+	regionGrowth(arr_out_img, centroids_st, &tot_stars);
+	printf("Total Stars: %d\n", tot_stars);
+	for(int i = 0; i < tot_stars; i++){
+		for(int j = 0; j < 3; j++){
+			printf("%Lf     ", centroids_st[i][j]);
+		}
+		printf("%d     \n", pixel_track[i]);
+	}
 	
 	/*cortos_printf("Image 2 output\n");
 	regionGrowth(arr_out_img_2, centroids_st);
