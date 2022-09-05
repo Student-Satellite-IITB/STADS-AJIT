563f5a7e3ebf8bb302809f165cd667032569c6fe
Nikhil Kaniyeri <69250757+kaniyeri@users.noreply.github.com>
2022-09-06 01:14:49 +0530

    Add files via upload

---
+++ RTOS/src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-led-blink
+ */
+
+// the code in setup function runs only one time when ESP32 starts
+#include "Arduino.h"
+#include "functions.h"
+#include "constants.h"
+#include "image_1.h"
+
+void setup() {
+  // initialize digital pin GIOP18 as an output.
+  pinMode(2, OUTPUT);
+  int sm_K_vec_arr[224792][3];
+
+printf("SIS_iter_1\n");
+OILS(arr_out_img_1);
+printf("\n\n\n");
+}
+
+// the code in loop function is executed repeatedly infinitely
+void loop() {
+
+}
