b9d317600b4be882212a018bab998bdedb554e84
puj24 <pujavedantu24@gmail.com>
2023-10-03 00:05:27 +0530

    Add OILS codes
    Added OILS codes with tested outputs on constellations & final quaternion error plots

---
+++ OILS/ORION/main.c
@@ -0,0 +1,618 @@
+// Standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+// User defined header files
+ #include "image_1.h"
+ #include "image_2.h" 
+ #include "image_3.h" 
+ #include "image_4.h" 
+ #include "image_5.h" 
+ #include "image_6.h" 
+ #include "image_7.h" 
+ #include "image_8.h" 
+ #include "image_9.h" 
+ #include "image_10.h"
+ #include "image_11.h"
+ #include "image_12.h"
+ #include "image_13.h"
+ #include "image_14.h"
+ #include "image_15.h"
+ #include "image_16.h"
+ #include "image_17.h"
+ #include "image_18.h"
+ #include "image_19.h"
+ #include "image_20.h"
+ #include "image_21.h"
+ #include "image_22.h"
+ #include "image_23.h"
+ #include "image_24.h"
+ #include "image_25.h"
+ #include "image_26.h"
+ #include "image_27.h"
+ #include "image_28.h"
+ #include "image_29.h"
+ #include "image_30.h"
+ #include "image_31.h"
+ #include "image_32.h"
+ #include "image_33.h"
+ #include "image_34.h"
+ #include "image_35.h"
+ #include "image_36.h"
+ #include "image_37.h"
+ #include "image_38.h"
+ #include "image_39.h"
+ #include "image_40.h"
+ #include "image_41.h"
+ #include "image_42.h"
+ #include "image_43.h"
+ #include "image_44.h"
+ #include "image_45.h"
+ #include "image_46.h"
+ #include "image_47.h"
+ #include "image_48.h"
+ #include "image_49.h"
+ #include "image_50.h"
+ #include "image_51.h"
+ #include "image_52.h"
+ #include "image_53.h"
+ #include "image_54.h"
+ #include "image_55.h"
+ #include "image_56.h"
+ #include "image_57.h"
+ #include "image_58.h"
+ #include "image_59.h"
+ #include "image_60.h"
+ #include "image_61.h"
+ #include "image_62.h"
+ #include "image_63.h"
+ #include "image_64.h"
+ #include "image_65.h"
+ #include "image_66.h"
+ #include "image_67.h"
+ #include "image_68.h"
+ #include "image_69.h"
+ #include "image_70.h"
+ #include "image_71.h"
+ #include "image_72.h"
+ #include "image_73.h"
+ #include "image_74.h"
+ #include "image_75.h"
+ #include "image_76.h"
+ #include "image_77.h"
+ #include "image_78.h"
+ #include "image_79.h"
+ #include "image_80.h"
+ #include "image_81.h"
+ #include "image_82.h"
+ #include "image_83.h"
+ #include "image_84.h"
+ #include "image_85.h"
+ #include "image_86.h"
+ #include "image_87.h"
+ #include "image_88.h"
+ #include "image_89.h"
+ #include "image_90.h"
+ #include "image_91.h"
+ #include "image_92.h"
+ #include "image_93.h"
+ #include "image_94.h"
+ #include "image_95.h"
+ #include "image_96.h"
+ #include "image_97.h"
+ #include "image_98.h"
+ #include "image_99.h"
+ #include "image_100.h"
+
+#include "functions.h"
+#include "constants.h"
+
+
+int sm_K_vec_arr[224792][3];
+
+
+int main()
+{
+
+printf("SIS_iter_1\n");
+OILS(arr_out_img_1);
+printf("\n\n\n");
+
+
+printf("SIS_iter_2\n");
+OILS(arr_out_img_2);
+printf("\n\n\n");
+
+
+printf("SIS_iter_3\n");
+OILS(arr_out_img_3);
+printf("\n\n\n");
+
+
+printf("SIS_iter_4\n");
+OILS(arr_out_img_4);
+printf("\n\n\n");
+
+
+printf("SIS_iter_5\n");
+OILS(arr_out_img_5);
+printf("\n\n\n");
+
+
+printf("SIS_iter_6\n");
+OILS(arr_out_img_6);
+printf("\n\n\n");
+
+
+printf("SIS_iter_7\n");
+OILS(arr_out_img_7);
+printf("\n\n\n");
+
+
+printf("SIS_iter_8\n");
+OILS(arr_out_img_8);
+printf("\n\n\n");
+
+
+printf("SIS_iter_9\n");
+OILS(arr_out_img_9);
+printf("\n\n\n");
+
+
+printf("SIS_iter_10\n");
+OILS(arr_out_img_10);
+printf("\n\n\n");
+
+
+printf("SIS_iter_11\n");
+OILS(arr_out_img_11);
+printf("\n\n\n");
+
+
+printf("SIS_iter_12\n");
+OILS(arr_out_img_12);
+printf("\n\n\n");
+
+
+printf("SIS_iter_13\n");
+OILS(arr_out_img_13);
+printf("\n\n\n");
+
+
+printf("SIS_iter_14\n");
+OILS(arr_out_img_14);
+printf("\n\n\n");
+
+
+printf("SIS_iter_15\n");
+OILS(arr_out_img_15);
+printf("\n\n\n");
+
+
+printf("SIS_iter_16\n");
+OILS(arr_out_img_16);
+printf("\n\n\n");
+
+
+printf("SIS_iter_17\n");
+OILS(arr_out_img_17);
+printf("\n\n\n");
+
+
+printf("SIS_iter_18\n");
+OILS(arr_out_img_18);
+printf("\n\n\n");
+
+
+printf("SIS_iter_19\n");
+OILS(arr_out_img_19);
+printf("\n\n\n");
+
+
+printf("SIS_iter_20\n");
+OILS(arr_out_img_20);
+printf("\n\n\n");
+
+
+printf("SIS_iter_21\n");
+OILS(arr_out_img_21);
+printf("\n\n\n");
+
+
+printf("SIS_iter_22\n");
+OILS(arr_out_img_22);
+printf("\n\n\n");
+
+
+printf("SIS_iter_23\n");
+OILS(arr_out_img_23);
+printf("\n\n\n");
+
+
+printf("SIS_iter_24\n");
+OILS(arr_out_img_24);
+printf("\n\n\n");
+
+
+printf("SIS_iter_25\n");
+OILS(arr_out_img_25);
+printf("\n\n\n");
+
+
+printf("SIS_iter_26\n");
+OILS(arr_out_img_26);
+printf("\n\n\n");
+
+
+printf("SIS_iter_27\n");
+OILS(arr_out_img_27);
+printf("\n\n\n");
+
+
+printf("SIS_iter_28\n");
+OILS(arr_out_img_28);
+printf("\n\n\n");
+
+
+printf("SIS_iter_29\n");
+OILS(arr_out_img_29);
+printf("\n\n\n");
+
+
+printf("SIS_iter_30\n");
+OILS(arr_out_img_30);
+printf("\n\n\n");
+
+
+printf("SIS_iter_31\n");
+OILS(arr_out_img_31);
+printf("\n\n\n");
+
+
+printf("SIS_iter_32\n");
+OILS(arr_out_img_32);
+printf("\n\n\n");
+
+
+printf("SIS_iter_33\n");
+OILS(arr_out_img_33);
+printf("\n\n\n");
+
+
+printf("SIS_iter_34\n");
+OILS(arr_out_img_34);
+printf("\n\n\n");
+
+
+printf("SIS_iter_35\n");
+OILS(arr_out_img_35);
+printf("\n\n\n");
+
+
+printf("SIS_iter_36\n");
+OILS(arr_out_img_36);
+printf("\n\n\n");
+
+
+printf("SIS_iter_37\n");
+OILS(arr_out_img_37);
+printf("\n\n\n");
+
+
+printf("SIS_iter_38\n");
+OILS(arr_out_img_38);
+printf("\n\n\n");
+
+
+printf("SIS_iter_39\n");
+OILS(arr_out_img_39);
+printf("\n\n\n");
+
+
+printf("SIS_iter_40\n");
+OILS(arr_out_img_40);
+printf("\n\n\n");
+
+
+printf("SIS_iter_41\n");
+OILS(arr_out_img_41);
+printf("\n\n\n");
+
+
+printf("SIS_iter_42\n");
+OILS(arr_out_img_42);
+printf("\n\n\n");
+
+
+printf("SIS_iter_43\n");
+OILS(arr_out_img_43);
+printf("\n\n\n");
+
+
+printf("SIS_iter_44\n");
+OILS(arr_out_img_44);
+printf("\n\n\n");
+
+
+printf("SIS_iter_45\n");
+OILS(arr_out_img_45);
+printf("\n\n\n");
+
+
+printf("SIS_iter_46\n");
+OILS(arr_out_img_46);
+printf("\n\n\n");
+
+
+printf("SIS_iter_47\n");
+OILS(arr_out_img_47);
+printf("\n\n\n");
+
+
+printf("SIS_iter_48\n");
+OILS(arr_out_img_48);
+printf("\n\n\n");
+
+
+printf("SIS_iter_49\n");
+OILS(arr_out_img_49);
+printf("\n\n\n");
+
+
+printf("SIS_iter_50\n");
+OILS(arr_out_img_50);
+printf("\n\n\n");
+
+
+printf("SIS_iter_51\n");
+OILS(arr_out_img_51);
+printf("\n\n\n");
+
+
+printf("SIS_iter_52\n");
+OILS(arr_out_img_52);
+printf("\n\n\n");
+
+
+printf("SIS_iter_53\n");
+OILS(arr_out_img_53);
+printf("\n\n\n");
+
+
+printf("SIS_iter_54\n");
+OILS(arr_out_img_54);
+printf("\n\n\n");
+
+
+printf("SIS_iter_55\n");
+OILS(arr_out_img_55);
+printf("\n\n\n");
+
+
+printf("SIS_iter_56\n");
+OILS(arr_out_img_56);
+printf("\n\n\n");
+
+
+printf("SIS_iter_57\n");
+OILS(arr_out_img_57);
+printf("\n\n\n");
+
+
+printf("SIS_iter_58\n");
+OILS(arr_out_img_58);
+printf("\n\n\n");
+
+
+printf("SIS_iter_59\n");
+OILS(arr_out_img_59);
+printf("\n\n\n");
+
+
+printf("SIS_iter_60\n");
+OILS(arr_out_img_60);
+printf("\n\n\n");
+
+
+printf("SIS_iter_61\n");
+OILS(arr_out_img_61);
+printf("\n\n\n");
+
+
+printf("SIS_iter_62\n");
+OILS(arr_out_img_62);
+printf("\n\n\n");
+
+
+printf("SIS_iter_63\n");
+OILS(arr_out_img_63);
+printf("\n\n\n");
+
+
+printf("SIS_iter_64\n");
+OILS(arr_out_img_64);
+printf("\n\n\n");
+
+
+printf("SIS_iter_65\n");
+OILS(arr_out_img_65);
+printf("\n\n\n");
+
+
+printf("SIS_iter_66\n");
+OILS(arr_out_img_66);
+printf("\n\n\n");
+
+
+printf("SIS_iter_67\n");
+OILS(arr_out_img_67);
+printf("\n\n\n");
+
+
+printf("SIS_iter_68\n");
+OILS(arr_out_img_68);
+printf("\n\n\n");
+
+
+printf("SIS_iter_69\n");
+OILS(arr_out_img_69);
+printf("\n\n\n");
+
+
+printf("SIS_iter_70\n");
+OILS(arr_out_img_70);
+printf("\n\n\n");
+
+
+printf("SIS_iter_71\n");
+OILS(arr_out_img_71);
+printf("\n\n\n");
+
+
+printf("SIS_iter_72\n");
+OILS(arr_out_img_72);
+printf("\n\n\n");
+
+
+printf("SIS_iter_73\n");
+OILS(arr_out_img_73);
+printf("\n\n\n");
+
+
+printf("SIS_iter_74\n");
+OILS(arr_out_img_74);
+printf("\n\n\n");
+
+
+printf("SIS_iter_75\n");
+OILS(arr_out_img_75);
+printf("\n\n\n");
+
+
+printf("SIS_iter_76\n");
+OILS(arr_out_img_76);
+printf("\n\n\n");
+
+
+printf("SIS_iter_77\n");
+OILS(arr_out_img_77);
+printf("\n\n\n");
+
+
+printf("SIS_iter_78\n");
+OILS(arr_out_img_78);
+printf("\n\n\n");
+
+
+printf("SIS_iter_79\n");
+OILS(arr_out_img_79);
+printf("\n\n\n");
+
+
+printf("SIS_iter_80\n");
+OILS(arr_out_img_80);
+printf("\n\n\n");
+
+
+printf("SIS_iter_81\n");
+OILS(arr_out_img_81);
+printf("\n\n\n");
+
+
+printf("SIS_iter_82\n");
+OILS(arr_out_img_82);
+printf("\n\n\n");
+
+
+printf("SIS_iter_83\n");
+OILS(arr_out_img_83);
+printf("\n\n\n");
+
+
+printf("SIS_iter_84\n");
+OILS(arr_out_img_84);
+printf("\n\n\n");
+
+
+printf("SIS_iter_85\n");
+OILS(arr_out_img_85);
+printf("\n\n\n");
+
+
+printf("SIS_iter_86\n");
+OILS(arr_out_img_86);
+printf("\n\n\n");
+
+
+printf("SIS_iter_87\n");
+OILS(arr_out_img_87);
+printf("\n\n\n");
+
+
+printf("SIS_iter_88\n");
+OILS(arr_out_img_88);
+printf("\n\n\n");
+
+
+printf("SIS_iter_89\n");
+OILS(arr_out_img_89);
+printf("\n\n\n");
+
+
+printf("SIS_iter_90\n");
+OILS(arr_out_img_90);
+printf("\n\n\n");
+
+
+printf("SIS_iter_91\n");
+OILS(arr_out_img_91);
+printf("\n\n\n");
+
+
+printf("SIS_iter_92\n");
+OILS(arr_out_img_92);
+printf("\n\n\n");
+
+
+printf("SIS_iter_93\n");
+OILS(arr_out_img_93);
+printf("\n\n\n");
+
+
+printf("SIS_iter_94\n");
+OILS(arr_out_img_94);
+printf("\n\n\n");
+
+
+printf("SIS_iter_95\n");
+OILS(arr_out_img_95);
+printf("\n\n\n");
+
+
+printf("SIS_iter_96\n");
+OILS(arr_out_img_96);
+printf("\n\n\n");
+
+
+printf("SIS_iter_97\n");
+OILS(arr_out_img_97);
+printf("\n\n\n");
+
+
+printf("SIS_iter_98\n");
+OILS(arr_out_img_98);
+printf("\n\n\n");
+
+
+printf("SIS_iter_99\n");
+OILS(arr_out_img_99);
+printf("\n\n\n");
+
+
+printf("SIS_iter_100\n");
+OILS(arr_out_img_100);
+printf("\n\n\n");
+    
+}
---
+++ OILS/README.md
@@ -0,0 +1,6 @@
+# STADS Algo Testing
+# OILS Results
+# Tuned epsilon in Est and TOL in SM 
+epsilon_seq_error = 0.005 //
+TOL = 0.5 but if(10e4* fabs < TOL) instead of (fabs < TOL/100)
+15 testcases with q_error > 36 arcsec error
